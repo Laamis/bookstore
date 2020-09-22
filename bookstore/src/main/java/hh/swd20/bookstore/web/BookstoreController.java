cea2be5c85d849e66eca19bfb193ad922b7d2ffd
Samuli Peltonen <samuli.peltonen1@gmail.com>
2020-09-22 18:00:56 +0300

    Added the possibility to delete data from the database via a link from the book-listing

--- bookstore/src/main/java/hh/swd20/bookstore/web/BookstoreController.java
+++ bookstore/src/main/java/hh/swd20/bookstore/web/BookstoreController.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -18,9 +19,17 @@ public class BookstoreController {
 	
 	@GetMapping("/index")
 	public String indexPage(Model model) {
-		
+		model.addAttribute("books", repo.findAll());		
 		return "bookstore";
 	}
 	
+	@GetMapping("/delete/{id}")
+	public String deleteStudent(@PathVariable("id") Long id, Model model) {
+		repo.deleteById(id);
+		
+		return "redirect:/index";
+	}
+	
+
 
 }
