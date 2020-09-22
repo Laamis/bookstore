a82d7a5900a5355855ca56bd65bea20346ed6b89
Samuli Peltonen <samuli.peltonen1@gmail.com>
2020-09-22 17:34:01 +0300

    Added JPA & H2 support to pom.xml, made some structural changes to domain's hierarchy, added a commandlinerunner, made book.java into an entity for db support

--- bookstore/src/main/java/hh/swd20/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/hh/swd20/bookstore/BookstoreApplication.java
@@ -1,13 +1,32 @@
 package hh.swd20.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.swd20.bookstore.domain.Book;
+import hh.swd20.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	//Commandlinerunner will add some test data to the db
+	@Bean
+	public CommandLineRunner demo(BookRepository repo) {
+		Book book1 = new Book("Harry Potter ja Azkabanin vanki", "J.K. Rowling", 2006, "123986127", 17.90);
+		Book book2 = new Book("Harry Potter ja kuoleman varjelukset", "J.K. Rowling", 2008, "12309785", 21.90);
+
+		// Let's save the test data to the db
+		repo.save(book1);
+		repo.save(book2);
+		//now you should be able to access the test-data
+		//with the endpoint h2-console
+			return null;
 	}
 
 }
--- bookstore/src/main/java/hh/swd20/bookstore/web/BookstoreController.java
+++ bookstore/src/main/java/hh/swd20/bookstore/web/BookstoreController.java
@@ -1,20 +1,26 @@
 package hh.swd20.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import domain.Book;
+
+import hh.swd20.bookstore.domain.Book;
+import hh.swd20.bookstore.domain.BookRepository;
 
 @Controller
 public class BookstoreController {
+	@Autowired
+	private BookRepository repo;
+	
 	@GetMapping("/index")
 	public String indexPage(Model model) {
-		// let's add thymeleaf stuff
-		Book hp1 = new Book("Harry Potter,", "J.K. Rowling", 2003, "12323-13234-21", 12.90);
-		model.addAttribute("book1", hp1.toString());
+		
 		return "bookstore";
 	}
+	
 
 }
